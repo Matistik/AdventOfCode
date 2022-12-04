5ac491d53741b6f2b192823a701fa68c64a22bdb
Matistik <xmatistikm@stuba.sk>
2022-12-04 21:06:50 +0100

    init + day 1

---
+++ AdventOfCode/src/main/java/com/example/adventofcode/day1/Day1.java
@@ -0,0 +1,42 @@
+package com.example.adventofcode.day1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day1 {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("/home/matistikoff/Documents/AdventOfCodeGit/AdventOfCode/AdventOfCode/src/main/java/com/example/adventofcode/day1/input.txt");
+
+        int elfCalories[] = new int[1000];
+        Integer elfNumber = 0;
+        Integer bigCaloryElf = 0;
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+
+        while ((st = br.readLine()) != null){
+
+            if (st.equals("")){
+                if (elfNumber == 0){
+                    bigCaloryElf = elfCalories[0];
+                }
+                else {
+                    if (elfCalories[elfNumber] > bigCaloryElf){
+                        bigCaloryElf = elfCalories[elfNumber];
+                    }
+                }
+                elfNumber++;
+                continue;
+            }
+            elfCalories[elfNumber] = elfCalories[elfNumber] + Integer.parseInt(st);
+        }
+
+        System.out.println(bigCaloryElf);
+
+    }
+}
